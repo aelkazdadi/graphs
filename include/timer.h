5587e0774d406a1ab95507ddf7efdebc8c4037fb
kazdadi <aamr.elkazdadi@gmail.com>
2019-03-23 19:19:55 +0100

    Timer refactor

--- include/timer.h
+++ include/timer.h
@@ -1,8 +1,8 @@
-#ifndef  TIMER_H
+#ifndef TIMER_H
 #define TIMER_H
 #include <time.h> //to estimate the runing time
 
-long long unsigned timeDiff(struct timespec *t1, struct timespec *t2);
+long long unsigned int timeDiff(struct timespec *t1, struct timespec *t2);
 
 void getTime(struct timespec *t);
 
--- src/timer.c
+++ src/timer.c
@@ -1,11 +1,8 @@
 #include "timer.h"
 
-void getTime(struct timespec *t)
-{
-  clock_gettime(CLOCK_MONOTONIC, t);
-}
+void getTime(struct timespec *t) { clock_gettime(CLOCK_MONOTONIC, t); }
 
-long long unsigned timeDiff(struct timespec *t1, struct timespec *t2) {
+long long unsigned int timeDiff(struct timespec *t1, struct timespec *t2) {
   return ((long long unsigned)(t2->tv_sec - t1->tv_sec)) * 1000000000 +
          (t2->tv_nsec - t1->tv_nsec);
 }
