4935ff54b7ee7a05712af2456d0ec7a8aea0c1c4
kazdadi <aamr.elkazdadi@gmail.com>
2019-03-23 20:28:55 +0100

    Single pass over text file of edges

--- src/ex5.c
+++ src/ex5.c
@@ -1,25 +1,54 @@
-#include <stdio.h>
 #include "degree.h"
-#include <time.h>
+#include "graph.h"
 #include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
-  struct timespec t1;
-  struct timespec t2;
+  struct timespec *clock = malloc(3 * sizeof(struct timespec));
+  unsigned long e1 = NLINKS;
+  unsigned long n1 = NLINKS;
 
-  getTime(&t1);
-
-  long unsigned int *degrees = getDegrees(argv[1])->degrees;
+  getTime(clock);
   FILE *file = fopen(argv[1], "r");
 
   long unsigned int s, t = 0;
   long long unsigned int Q = 0;
-  while (fscanf(file, "%lu%lu", &s, &t) == 2) {
-      Q += degrees[s - 1] * degrees[t - 1];
-  }
-  fclose(file);
 
-  getTime(&t2);
+  long unsigned int *degrees = malloc(e1 * sizeof(long unsigned int));
+  edgelist *g = malloc(sizeof(edgelist));
+  g->n = 0;
+  g->e = 0;
+  g->edges = malloc(e1*sizeof(edge));
+  while (fscanf(file, "%lu%lu", &s, &t) == 2) {
+    g->n = max3(g->n, s, t);
+    --s;
+    --t;
+    g->edges[g->e].s = s;
+    g->edges[g->e].t = t;
+    ++(degrees[s]);
+    ++(degrees[t]);
+
+    if (g->e++ == e1) { // increase allocated RAM if needed
+      e1 += NLINKS;
+      g->edges = realloc(g->edges, e1 * sizeof(edge));
+    }
+    if (g->n == n1) { // increase allocated RAM if needed
+      n1 += NLINKS;
+      degrees = realloc(degrees, n1 * sizeof(long unsigned int));
+    }
+  }
+
+  fclose(file);
+  getTime(clock + 1);
+
+  for (long unsigned int i = 0; i < g->e; ++i)
+  {
+    Q += degrees[g->edges[i].s] * degrees[g->edges[i].t];
+  }
+
+  getTime(clock + 2);
   printf("Q_G: %llu\n", Q);
-  printf("Time taken: %Lf\n", timeDiff(&t1, &t2)/1000000000.0L);
+  printf("Time taken: %Lf\n", timeDiff(clock, clock + 1) / 1000000000.0L);
+  printf("Time taken: %Lf\n", timeDiff(clock + 1, clock + 2) / 1000000000.0L);
 }
