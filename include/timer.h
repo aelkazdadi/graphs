b360912de63729022db31d2b6048c81d0bc98379
kazdadi <aamr.elkazdadi@gmail.com>
2019-03-24 13:45:18 +0100

    Added conversion factor from nanoseconds to seconds

--- include/timer.h
+++ include/timer.h
@@ -1,5 +1,7 @@
 #ifndef TIMER_H
 #define TIMER_H
+#define NSEC_IN_SEC 1000000000.0L
+
 #include <time.h> //to estimate the runing time
 
 long long unsigned int timeDiff(struct timespec *t1, struct timespec *t2);
