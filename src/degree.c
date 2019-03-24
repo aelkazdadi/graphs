d1f49a135db3900ba63a1e238dc4b2ef783256f4
kazdadi <aamr.elkazdadi@gmail.com>
2019-03-24 13:06:02 +0100

    Updated degree list to work with cleaned files

--- src/degree.c
+++ src/degree.c
@@ -4,28 +4,21 @@
 #include <stdlib.h>
 
 degreeList *getDegrees(char *input) {
-  unsigned long n1 = NLINKS;
   FILE *file = fopen(input, "r");
 
-  // allocate some RAM to store degrees
   degreeList *out = malloc(sizeof(degreeList));
-  out->n = 0;
-  out->degrees = malloc(n1 * sizeof(unsigned long int));
+
+  unsigned long int tmp;
+  if (fscanf(file, "%lu%lu", &(out->n), &tmp) != 2) return NULL;
+
+  out->degrees = malloc((out->n) * sizeof(unsigned long int));
 
   unsigned long int s, t = 0;
   while (fscanf(file, "%lu%lu", &s, &t) == 2) {
-    out->n = max3(out->n, s, t);
-    ++out->degrees[s - 1];
-    ++out->degrees[t - 1];
-
-    if (out->n + 1 == n1) { // increase allocated RAM if needed
-      n1 += NLINKS;
-      out->degrees = realloc(out->degrees, n1 * sizeof(unsigned long int));
-    }
+    ++out->degrees[s];
+    ++out->degrees[t];
   }
   fclose(file);
-
-  out->degrees = realloc(out->degrees, out->n * sizeof(unsigned long int));
 
   return out;
 }
