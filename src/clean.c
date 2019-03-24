ddddcd292d1f99efd3bce8014db65f4c6cc58963
kazdadi <aamr.elkazdadi@gmail.com>
2019-03-24 02:32:34 +0100

    Cleaning unsorted edge lists

--- src/clean.c
+++ src/clean.c
@@ -1,58 +1,94 @@
 #include "graph.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
-void clean(char *input, char *output) {
-  printf("%s\n", input);
-  printf("%s\n", output);
+int compare(const void* a, const void* b) {
+  edge *e1 = (edge *) a;
+  edge *e2 = (edge *) b;
+  return 2 * ((e1->s > e2->s) - (e1->s < e2->s)) +
+         ((e1->t > e2->t) - (e1->t < e2->t));
+}
 
-  FILE *in = fopen(input, "r");
-  FILE *out = fopen(output, "w");
+int main(int argc, char **argv) {
+  printf("Input file: %s\n", argv[1]);
+  printf("Output file: %s\n", argv[2]);
 
-  if (in == NULL || out == NULL)
-    return ;
+  // Count lines in input file
+  char command[10000];
+  strcpy(command, "wc -l ");
+  strcat(command, argv[1]);
+  printf("Running \"%s\"...\n", command);
+  char buffer[128];
+  FILE *fp;
+  fp = popen(command, "r");
+  long unsigned int nLines;
+  if (fgets(buffer, 128, fp) != NULL) {
+    nLines = strtoul(buffer, NULL, 10);
+  }
+  pclose(fp);
+  printf("%lu lines found.\n", nLines);
 
+  // Count comments
   char *line = NULL;
   size_t size = 0;
   ssize_t getLineResult;
   int scanResult;
 
-  unsigned int nComments = 0;
-  long long unsigned int e1 = NLINKS;
+  unsigned int nComments = -1;
+  FILE *in = fopen(argv[1], "r");
+  if (in == NULL)
+    return -1;
 
   do {
     getLineResult = getline(&line, &size, in);
     ++nComments;
   } while (getLineResult > 0 && line[0] == '#');
-  --nComments;
+  printf("%u comments found.\n", nComments);
 
-  unsigned long s, t = 0;
+  // Store edges in memory
+  long unsigned int n = 0;
+  long unsigned int e = 0;
 
-  edgelist *g = malloc(sizeof(edgelist));
-  g->n = 0;
-  g->e = 0;
-  g->edges = malloc(e1 * sizeof(edge));
+  edge *edges = malloc(sizeof(edge) * (nLines - nComments));
+
+  long unsigned s, t = 0;
 
   scanResult = sscanf(line, "%lu%lu", &s, &t);
+  free(line);
   while (scanResult == 2) {
-    if (s < t) {
-      g->n = max3(g->n, s, t);
-      g->edges[g->e].s = s;
-      g->edges[g->e].t = t;
-      fprintf(out, "%lu %lu\n", s, t);
-
-      if (g->e++ == 1) {
-        e1 += NLINKS;
-        g->edges = realloc(g->edges, e1 * sizeof(edge));
-      }
+    n = max3(n, s, t);
+    if (s > t) {
+      long unsigned int tmp = s;
+      s = t;
+      t = tmp;
+    }
+    if (s != t) {
+      // Discard self loops
+      edges[e].s = s - 1;
+      edges[e].t = t - 1;
+      ++e;
     }
     scanResult = fscanf(in, "%lu%lu", &s, &t);
   }
   fclose(in);
+  if (e == 0) return -1;
+
+  // Free unneeded memory
+  edges = realloc(edges, e * sizeof(edge));
+
+  // Sort edges
+  qsort(edges, e, sizeof(edge), compare);
+
+  // Write edges to output file
+  FILE *out = fopen(argv[2], "w");
+  fprintf(out, "%lu %lu\n", n, e);
+
+  fprintf(out, "%lu %lu\n", edges[0].s, edges[0].t);
+  for (long unsigned int i = 1; i < e; ++i) {
+    if (compare(edges+i, edges+(i-1)) > 0)
+      fprintf(out, "%lu %lu\n", edges[i].s, edges[i].t);
+  }
   fclose(out);
-
-  printf("%lu %lu\n", g->n, g->e);
 }
-
-int main(int argc, char **argv) { clean(argv[1], argv[2]); }
