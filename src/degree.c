0fcd55812fab223054f0250a8ee0ea586d2cf29e
kazdadi <aamr.elkazdadi@gmail.com>
2019-03-24 13:44:32 +0100

    Initialized degrees to zero

--- src/degree.c
+++ src/degree.c
@@ -3,24 +3,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-degreeList *getDegrees(char *input) {
-  FILE *file = fopen(input, "r");
+degreeList* getDegrees(char* input) {
+  FILE* file = fopen(input, "r");
 
-  degreeList *out = malloc(sizeof(degreeList));
+  degreeList* out = malloc(sizeof(degreeList));
 
   unsigned long int tmp;
   if (fscanf(file, "%lu%lu", &(out->n), &tmp) != 2) return NULL;
 
   out->degrees = malloc((out->n) * sizeof(unsigned long int));
 
+  for (long unsigned int i = 0; i < out->n; ++i) {
+    out->degrees[i] = 0;
+  }
+
   unsigned long int s, t = 0;
   while (fscanf(file, "%lu%lu", &s, &t) == 2) {
-    ++out->degrees[s];
-    ++out->degrees[t];
+    ++(out->degrees[s]);
+    ++(out->degrees[t]);
   }
   fclose(file);
 
   return out;
 }
 
-void freeDegreeList(degreeList *degrees) { free(degrees->degrees); }
+void freeDegreeList(degreeList* degrees) { free(degrees->degrees); }
