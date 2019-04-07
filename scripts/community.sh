3bb0b93f7873f152ada5806c645e682a6b63137c
El Kazdadi <aamr.elkazdadi@gmail.com>
2019-04-07 14:24:04 +0200

    Added scripts

---
+++ scripts/community.sh
@@ -0,0 +1,5 @@
+echo ""
+echo ""
+
+mkdir -p "${output_dir}/community"
+python3 "${build_dir}/community.py" "${output_dir}/community/"
---
+++ scripts/init.sh
@@ -0,0 +1,14 @@
+declare -a graphs=(
+                   "email-Eu-core"
+                   "com-amazon.ungraph"
+                   "com-lj.ungraph"
+                   "com-orkut.ungraph"
+                   "com-friendster.ungraph"
+                   )
+
+pagerank_graph="alr21--dirLinks--enwiki-20071018"
+
+build_dir="/home/aamr/graphs/build/"
+
+input_dir="/media/aamr/HDD/data/raw/"
+output_dir="/media/aamr/HDD/data/"
