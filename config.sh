09e3bfc2d0d1e31a21c963e7c88f65bc396c30cf
El Kazdadi <aamr.elkazdadi@gmail.com>
2019-04-07 15:55:49 +0200

    Generic paths

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ declare -a graphs=(
 
 pagerank_graph="alr21--dirLinks--enwiki-20071018"
 
-build_dir="/home/aamr/graphs/build/"
+build_dir="./build"
 
-input_dir="/media/aamr/HDD/data/raw/"
-output_dir="/media/aamr/HDD/data/"
+input_dir="directory/containing/edgelists"
+output_dir="directory/to/output/program/results"
